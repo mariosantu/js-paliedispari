7f5a283eccfa13d2cc1b03f5b1e5d724fc57f526
Mario Santucci <mario.santucci95@gmail.com>
2021-03-25 17:00:16 +0100

    inizio parole palindrome

---
+++ palindromi/js/script.js
@@ -0,0 +1,38 @@
+//scrivere una funzione per capire se una parola è palindroma
+
+//chiedo la parola all'utente
+//alert('Benvenuto qui potrai scoprire se una parola è palindroma o meno. Premi invio per iniziare')
+
+var parolaUtente = prompt('inserisci qui la parola da verificare : ');
+console.log(parolaUtente);
+
+
+for ( var i = 0; i < parolaUtente.length; i++) {
+    var parolaSeparataOrd = parolaUtente[i];
+    console.log(parolaSeparataOrd);
+}
+
+
+for ( var i = parolaUtente.length -1; i >= 0; i--) {
+    var parolaSeparataDis = parolaUtente[i];
+    console.log(parolaUtente[i]);
+}
+
+
+function getPalindrome (parolaDaValutare) {
+    
+    var parolaValutata = 
+
+    
+    
+    return parolaValutata;
+}
+
+
+
+
+
+
+
+
+
